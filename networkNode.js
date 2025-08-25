b19560de807bb1ff7cdb284957988d68155147f3
Mahdi Khosravani <mehdikhosravani61@gmail.com>
2025-08-25 09:27:08 +0330

    Update networkNode.js

--- networkNode.js
+++ networkNode.js
@@ -2,93 +2,59 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
-const { v1: uuidv1 } = require('uuid');
-const port = process.argv[2];
 const rp = require('request-promise');
 
-const nodeAddress = uuidv1().split('-').join('');
-const blockchain = new Blockchain();
+const port = process.argv[2];
+const nodeAddress = require('uuid').v1().split('-').join('');
+
+const bitcoin = new Blockchain();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// گرفتن کل بلاکچین
 app.get('/blockchain', function (req, res) {
-  res.send(blockchain);
+  res.send(bitcoin);
 });
 
-// Create transaction for scenario
-app.post('/transaction/broadcast', function(req, res) {
-  const { scenarioType, details } = req.body;
-  const newTransaction = blockchain.createNewTransaction(scenarioType, details);
-  blockchain.addTransactionToPendingTransactions(newTransaction);
+// تغییر اصلی: API برای ایجاد ترنزکشن با متادیتا
+app.post('/transaction/broadcast', function (req, res) {
+  const newTransaction = bitcoin.createNewTransaction(
+    req.body.scenarioType,
+    req.body.region,
+    req.body.sorting,
+    req.body.itemId,
+    req.body.details
+  );
 
-  const requestPromises = [];
-  blockchain.networkNodes.forEach(networkNodeUrl => {
-    const requestOptions = {
-      url: networkNodeUrl + '/transaction',
-      method: 'POST',
-      body: newTransaction,
-      json: true
-    };
-    requestPromises.push(rp(requestOptions));
-  });
-
-  Promise.all(requestPromises)
-  .then(data => {
-    res.json({ note: `Transaction of type ${scenarioType} created and broadcast successfully.`, transaction: newTransaction });
-  });
+  res.json({ note: 'Transaction created and broadcast successfully.' });
 });
 
-app.post('/transaction', function(req, res) {
-  const newTransaction = req.body;
-  const blockIndex = blockchain.addTransactionToPendingTransactions(newTransaction);
-  res.json({ note: `Transaction will be added in block ${blockIndex}.` });
-});
-
-app.get('/mine', function(req, res) {
-  const lastBlock = blockchain.getLastBlock();
+// ماین کردن بلاک جدید
+app.get('/mine', function (req, res) {
+  const lastBlock = bitcoin.getLastBlock();
   const previousBlockHash = lastBlock['hash'];
+
   const currentBlockData = {
-    transactions: blockchain.pendingTransactions,
+    transactions: bitcoin.pendingTransactions,
     index: lastBlock['index'] + 1
   };
-  const nonce = blockchain.proofOfWork(previousBlockHash, currentBlockData);
-  const blockHash = blockchain.hashBlock(previousBlockHash, currentBlockData, nonce);
 
-  const newBlock = blockchain.createNewBlock(nonce, previousBlockHash, blockHash);
+  const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+  const blockHash = bitcoin.hashBlock(
+    previousBlockHash,
+    currentBlockData,
+    nonce
+  );
 
-  const requestPromises = [];
-  blockchain.networkNodes.forEach(networkNodeUrl => {
-    const requestOptions = {
-      url: networkNodeUrl + '/receive-new-block',
-      method: 'POST',
-      body: { newBlock: newBlock },
-      json: true
-    };
-    requestPromises.push(rp(requestOptions));
-  });
+  const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
 
-  Promise.all(requestPromises)
-  .then(data => {
-    res.json({ note: "New block mined & broadcast successfully", block: newBlock });
+  res.json({
+    note: 'New block mined & broadcast successfully',
+    block: newBlock
   });
 });
 
-app.post('/receive-new-block', function(req, res) {
-  const newBlock = req.body.newBlock;
-  const lastBlock = blockchain.getLastBlock();
-  const correctHash = lastBlock.hash === newBlock.previousBlockHash;
-  const correctIndex = lastBlock['index'] + 1 === newBlock['index'];
-
-  if (correctHash && correctIndex) {
-    blockchain.chain.push(newBlock);
-    blockchain.pendingTransactions = [];
-    res.json({ note: 'New block received and accepted.', newBlock: newBlock });
-  } else {
-    res.json({ note: 'New block rejected.', newBlock: newBlock });
-  }
-});
-
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}...`);
 });
